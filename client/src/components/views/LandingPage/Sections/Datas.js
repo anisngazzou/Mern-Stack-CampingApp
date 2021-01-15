eea2d8832cd545c6665b7bd2f7359b98d01b4e09
unknown <nole.anis@gmail.com>
2021-01-15 18:31:59 +0100

    first commit

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,76 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "0 to 199 Dt",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "200 to 249 Dt",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "250 to 279 Dt",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": " 280 to  299 Dt",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than  300 Dt",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Nord"
+    },
+    {
+        "_id": 2,
+        "name": "Nord-Est"
+    },
+    {
+        "_id": 3,
+        "name": "Nord-Ouest"
+    },
+    {
+        "_id": 4,
+        "name": "Sud-Est"
+    },
+    {
+        "_id": 5,
+        "name": "Sud "
+    },
+    {
+        "_id": 6,
+        "name": "Sud-Ouest"
+    },
+    {
+        "_id": 7,
+        "name": "Ouest"
+    },
+    {
+        "_id": 8,
+        "name": "Est"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://localhost:27017/webpro'
+    //'mongodb+srv://<ngazouanis>:<1994>@cluster0-f2ggq.mongodb.net/webpro?retryWrites=true&w=majority'
+    //process.env.MONGO_URI POUR HOSTT 
+}
